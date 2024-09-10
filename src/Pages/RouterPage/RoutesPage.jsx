65aa20406de818adf557e70647f7b99c6e37952c
MahmudovSoltan <mahmudovsoltan477@gmail.com>
2024-09-10 11:46:57 +0400

    variablesSCSS and Router

---
+++ src/Pages/RouterPage/RoutesPage.jsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import LeyotPages from "../LeyoutPage/LeyotPages";
+
+const RoutesPage = () => {
+  return (
+    <div>
+      <LeyotPages>
+        <Routes>
+            
+        </Routes>
+      </LeyotPages>
+    </div>
+  );
+};
+
+export default RoutesPage;

78923aa4c08bc7d3deca404a865690c1788d3436
Nasiba1719 <salmanli.91@mail.ru>
2024-09-11 18:44:29 +0400

    Merge oldu mainle

--- src/Pages/RouterPage/RoutesPage.jsx
+++ src/Pages/RouterPage/RoutesPage.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import LayoutPages from "../LayoutPage/LayoutPages";
 import Welcome from "../WelcomeRegisterPage/WelcomeRegisterPage";
+import HomePage from "../HomePage/HomePage";
 
 const RoutesPage = () => {
   return (
     <div>
       <LayoutPages>
         <Routes>
+          <Route path="/" element={<HomePage />} />
           <Route path="/welcome" element={<Welcome />} />
         </Routes>
       </LayoutPages>

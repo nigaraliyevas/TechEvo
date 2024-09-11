286528c634ab790adca5b5091fbc67395eb08aeb
lemanshamilova <lemansamilova2003@gmail.com>
2024-09-11 18:26:48 +0400

    hello

--- src/Pages/RouterPage/RoutesPage.jsx
+++ src/Pages/RouterPage/RoutesPage.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import LayoutPages from "../LayoutPage/LayoutPages";
+import WelcomeRegisterPage from "../WelcomeRegisterPage/WelcomeRegisterPage";
 
 const RoutesPage = () => {
   return (
     <div>
       <LayoutPages>
         <Routes>
+          <Route path="/login" element={<WelcomeRegisterPage/>} />
             
         </Routes>
       </LayoutPages>

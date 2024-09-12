b2e5880374ccce33523a47e62499f844986caa7b
nigaraliyevas <naliyeva965@gmail.com>
2024-09-12 17:27:52 +0400

    added passwordResetPage and changed welcomeRegisterPage button

--- src/Pages/RouterPage/RoutesPage.jsx
+++ src/Pages/RouterPage/RoutesPage.jsx
@@ -2,7 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import LayoutPages from "../LayoutPage/LayoutPages";
 import Welcome from "../WelcomeRegisterPage/WelcomeRegisterPage";
 import ForgetPassPage from "../ForgetPasswordPage/ForgetPassPage";
-
+import PasswordReset from "../PasswordResetPage/PasswordReset";
 
 const RoutesPage = () => {
   return (
@@ -11,6 +11,7 @@ const RoutesPage = () => {
         <Routes>
           <Route path="/welcome" element={<Welcome />} />
           <Route path="/forget" element={<ForgetPassPage />} />
+          <Route path="/newpassword" element={<PasswordReset />} />
         </Routes>
       </LayoutPages>
     </div>

f76cef209b0633ee10ca0f2a558f225182d29dc0
nigaraliyevas <naliyeva965@gmail.com>
2024-09-12 19:44:04 +0400

    EnterPasswordPage is done

--- src/pages/WelcomeRegisterPage/WelcomeRegisterPage.jsx
+++ src/pages/WelcomeRegisterPage/WelcomeRegisterPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import  { useState } from "react";
 import styles from "./WelcomeRegisterPage.module.scss";
 import "../../components/css/Button.scss";
 import eyeIcon from "../../../public/assets/images/Welcome/Faeye.png";

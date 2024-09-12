544c8684205acb2f04551c2b688daa8103a2cfee
Emil477 <emil_salman477@mail.ru>
2024-09-12 13:10:53 -0700

    Merge

--- src/Pages/RouterPage/RoutesPage.jsx
+++ src/Pages/RouterPage/RoutesPage.jsx
@@ -1,6 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 import LayoutPages from "../LayoutPage/LayoutPages";
 import Welcome from "../WelcomeRegisterPage/WelcomeRegisterPage";
+import ForgetPassPage from "../ForgetPasswordPage/ForgetPassPage";
+
 
 const RoutesPage = () => {
   return (
@@ -8,6 +10,7 @@ const RoutesPage = () => {
       <LayoutPages>
         <Routes>
           <Route path="/welcome" element={<Welcome />} />
+          <Route path="/forget" element={<ForgetPassPage />} />
         </Routes>
       </LayoutPages>
     </div>

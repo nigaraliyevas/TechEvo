2d81f7b09884002d85ce5b1447e0ddb38ad351c7
MahmudovSoltan <mahmudovsoltan477@gmail.com>
2024-09-13 18:13:14 +0400

    my commit

--- src/Pages/LayoutPage/HeaderPage/HeaderPage.jsx
+++ src/Pages/LayoutPage/HeaderPage/HeaderPage.jsx
@@ -5,39 +5,65 @@ import { BiUser } from "react-icons/bi";
 import { GoRocket } from "react-icons/go";
 
 const HeaderPage = () => {
-
   return (
+    <header >
+         <div className={`${styles.container} ${styles.navbar_bg} `}>
+        <nav className={styles.navbar}>
+          <div className={styles.navbar_title}>TechEvo</div>
+          <div className={styles.navbar_right}>
+            <ul className={styles.navbar_pages}>
+              <li className={styles.navbar_links}>
+                Pc
+                <div className={styles.link_catagiries}>
+                  <div>
+                    <ul>
+                      <li> <span>•</span> Oyun üçün</li>
+                      <li> <span>•</span> Dizayn üçün</li>
+                      <li> <span>•</span> Ofis üçün</li>
+                      <li> <span>•</span> Ev üçün</li>
+                      <li> <span>•</span> Hamısı</li>
+                    </ul>
+                  </div>
+                </div>
+              </li>
+              <li className={styles.navbar_links}>Laptop</li>
+              <li className={styles.navbar_links}>Aksesuarlar
+              <div className={styles.link_catagiries}>
+                  <div>
+                    <ul>
+                      <li> <span>•</span> Oyun üçün</li>
+                      <li> <span>•</span> Dizayn üçün</li>
+                      <li> <span>•</span> Ofis üçün</li>
+                      <li> <span>•</span> Ev üçün</li>
+                      <li> <span>•</span> Hamısı</li>
+                    </ul>
+                  </div>
+                </div>
+              </li>
+              <li className={styles.navbar_ideal}>
+                Ideal Pc <GoRocket size={24} />
+              </li>
+              <li className={styles.navbar_links}>Xidmetler</li>
+              <li className={styles.navbar_links}>Blog</li>
+              <li className={styles.navbar_links}>Dəstək</li>
+            </ul>
 
-    <header>
-    <div className={`${styles.container} ${styles.navbar_bg} `}>
-      <nav className={styles.navbar}>
-        <div className={styles.navbar_title}>TechEvo</div>
-        <div className={styles.navbar_right}>
-        <ul className={styles.navbar_pages}>
-          <li className={styles.navbar_links}>Pc</li>
-          <li className={styles.navbar_links}>Laptop</li>
-          <li className={styles.navbar_links}>Aksesuarlar</li>
-          <li className={styles.navbar_ideal}>Ideal Pc <GoRocket size={24}/></li>
-          <li className={styles.navbar_links}>Xidmetler</li>
-          <li className={styles.navbar_links}>Blog</li>
-          <li className={styles.navbar_links}>Dəstək</li>
-        </ul>
+            <div className={styles.navbar_icons}>
+              <div>
+                <CiSearch size={24} />
+              </div>
+              <div>
+                <SlBasket size={24} />
+              </div>
+              <div>
+                <BiUser size={24} />
+              </div>
+            </div>
+          </div>
+        </nav>
 
-        <div className={styles.navbar_icons}>
-          <div>
-            <CiSearch  size={24}/>
-          </div>
-          <div>
-            <SlBasket  size={24}/>
-          </div>
-          <div>
-            <BiUser  size={24}/>
-          </div>
-        </div>
+         </div>
 
-        </div>
-      </nav>
-    </div>
     </header>
   );
 };

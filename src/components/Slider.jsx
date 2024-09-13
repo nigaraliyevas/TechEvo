4c64277c5bc1c0a6443627eefe7953f727aa25a6
Nasiba1719 <salmanli.91@mail.ru>
2024-09-13 18:42:00 +0400

    ready

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -13,7 +13,7 @@ const SliderComponent = () => {
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 2500,
+        autoplaySpeed: 4000,
         fade: true,
     };
 
@@ -21,7 +21,7 @@ const SliderComponent = () => {
         <div className={styles.sliderContainer}>
             <Slider arrows={false} {...settings} >
                 <div className={styles.slide}>
-                    <div style={{ position: "absolute", left: "10%" }} className={styles.container_top}>
+                    <div  className={styles.container_top}>
                         <div className={styles.mainTitle}>
                             <h1>Empower Your Digital World</h1>
                             <p>Find the perfect computer, PC, or laptop for unbeatable performance</p>
@@ -30,7 +30,7 @@ const SliderComponent = () => {
                     <img style={{ width: "100%", height: "100%" }} src={Slide1} alt="Slide 1" />
                 </div>
                 <div className={styles.slide}>
-                    <div style={{ position: "absolute", left: "10%" }} className={styles.container_top}>
+                    <div  className={styles.container_top}>
                         <div className={styles.mainTitle}>
                             <h1>Laptops and PCs Built for You</h1>
                             <p>Upgrade with our top-tier computers, PCs, and laptops</p>
@@ -39,7 +39,7 @@ const SliderComponent = () => {
                     <img src={Slide2} alt="Slide 2" />
                 </div>
                 <div className={styles.slide}>
-                    <div style={{ position: "absolute", left: "10%" }} className={styles.container_top}>
+                    <div  className={styles.container_top}>
                         <div className={styles.mainTitle}>
                             <h1>Discover Your Perfect PC</h1>
                             <p>Upgrade with our top-tier computers,PCs, and laptops.</p>

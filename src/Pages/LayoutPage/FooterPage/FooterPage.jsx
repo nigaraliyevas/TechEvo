761b31ac3b175a60dc0ffa50f6104f3593ceca05
nigaraliyevas <naliyeva965@gmail.com>
2024-09-24 01:38:59 +0400

    added links to header and footer

--- src/Pages/LayoutPage/FooterPage/FooterPage.jsx
+++ src/Pages/LayoutPage/FooterPage/FooterPage.jsx
@@ -1,27 +1,30 @@
 import style from "./Footer.module.scss";
 import { FaInstagram, FaTwitter, FaFacebook } from "react-icons/fa";
 import { FiPhone } from "react-icons/fi";
+import { Link } from "react-router-dom";
 
 const FooterPage = () => {
   return (
     <footer className={`${style.container} ${style.footer}`}>
       <div className={style.footer_top}>
-        <div className={style.footer_title}>TechEvo</div>
+        <div className={style.footer_title}>
+          <Link className="text-white text-decoration-none" to="/">
+            TechEvo
+          </Link>
+        </div>
         <div className={style.footer_icons}>
           <span className={style.footer_icon}>
-            <FaInstagram size={24}/>
+            <FaInstagram size={24} />
           </span>
           <span className={style.footer_icon}>
-            <FaTwitter size={24}/>
+            <FaTwitter size={24} />
           </span>
           <span className={style.footer_icon}>
-            <FaFacebook size={24}/>
+            <FaFacebook size={24} />
           </span>
         </div>
       </div>
-      <p className={style.footer_text}>
-        Design amazing digital experiences that create more happy in the world.
-      </p>
+      <p className={style.footer_text}>Design amazing digital experiences that create more happy in the world.</p>
       <ul className={style.footer_links}>
         <li>Haqqımızda</li>
         <li>Şərtlər və Qaydalar</li>
--- src/Pages/LayoutPage/HeaderPage/HeaderPage.jsx
+++ src/Pages/LayoutPage/HeaderPage/HeaderPage.jsx
@@ -7,27 +7,25 @@ import { LiaUserPlusSolid } from "react-icons/lia";
 import { PiSignInFill } from "react-icons/pi";
 import { IoSettingsOutline } from "react-icons/io5";
 import { IoMdMenu } from "react-icons/io";
-import { useNavigate } from "react-router-dom";
-import navBarLogo from "../../../../public/assets/images/HeaderPage/tech-evo-logo 1.png"
-
-
-
+import { Link, useNavigate } from "react-router-dom";
+import navBarLogo from "../../../../public/assets/images/HeaderPage/tech-evo-logo 1.png";
 
 const HeaderPage = () => {
- const navigate=useNavigate()
+  const navigate = useNavigate();
 
   return (
     <header>
       <div className={`${styles.container} ${styles.navbar_bg} `}>
         <nav className={styles.navbar}>
-          <div className={styles.navbar_logo}><img src={navBarLogo} alt="navBarLogo" /></div>
+          <div className={styles.navbar_logo}>
+            <Link to="/">
+              <img src={navBarLogo} alt="navBarLogo" />
+            </Link>
+          </div>
           <div className={styles.navbar_right}>
             <ul className={styles.navbar_pages}>
-              <li
-                style={{ padding: "0px 20px" }}
-                className={styles.navbar_links}
-              >
-                Pc 
+              <Link to={"/pc"} style={{ padding: "0px 20px" }} className={`${styles.navbar_links}  text-decoration-none text-white`}>
+                Pc
                 <div className={styles.link_catagiries}>
                   <div>
                     <ul>
@@ -49,7 +47,7 @@ const HeaderPage = () => {
                     </ul>
                   </div>
                 </div>
-              </li>
+              </Link>
               <li className={styles.navbar_links}>Laptop</li>
               <li className={styles.navbar_links}>
                 Aksesuarlar
@@ -83,12 +81,11 @@ const HeaderPage = () => {
             <div className={styles.navbar_icons}>
               <div className={styles.navbar_icon}>
                 <FiSearch size={24} />
-
               </div>
-              <div className={styles.navbar_icon} >
+              <div className={styles.navbar_icon}>
                 <SlBasket size={24} />
               </div>
-              <div className={styles.navbar_users } style={{padding:"32px 0px"}}>
+              <div className={styles.navbar_users} style={{ padding: "32px 0px" }}>
                 <BiUser size={24} />
                 <div className={styles.navbar_users_items}>
                   <div className={styles.navbar_users_item}>
@@ -99,13 +96,13 @@ const HeaderPage = () => {
                   </div>
                   <div className={styles.navbar_users_item}>
                     <span>
-                      <PiSignInFill size={24}  />
+                      <PiSignInFill size={24} />
                     </span>
                     Daxil ol
                   </div>
                   <div className={styles.navbar_users_item}>
                     <span>
-                      <BiUser size={24}  />
+                      <BiUser size={24} />
                     </span>
                     Hesabım
                   </div>

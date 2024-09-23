6c64799e08d95778fb5991b72dd99002f429cb4b
nigaraliyevas <naliyeva965@gmail.com>
2024-09-24 00:46:35 +0400

    last chanhges of search  branch merged with main

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(({ mode }) => {
   return {
     plugins: [react()],
-    base: mode === "production" ? "/TechEvo/" : "/", // Set base URL based on mode
   };
 });

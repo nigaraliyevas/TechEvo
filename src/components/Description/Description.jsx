2f72056655cd7e073343e8a58b6bf643b3121de3
Nasiba1719 <salmanli.91@mail.ru>
2024-09-24 16:19:12 +0400

    start desc

---
+++ src/components/Description/Description.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from './Description.module.scss'
+
+const Description = () => {
+  return (
+    <div className={style.container}>
+
+    </div>
+  )
+}
+
+export default Description

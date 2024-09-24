339e2211b88c8707408213afd3c99f161142173d
lemanshamilova <lemansamilova2003@gmail.com>
2024-09-24 16:13:05 +0400

    grid system is created

---
+++ src/Pages/ProductPage/ProductPage.jsx
@@ -0,0 +1,44 @@
+import styles from "./ProductPage.module.scss";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+const ProductPage = () => {
+  return (
+    <section id="product">
+      <div className="container">
+        <div className={styles.productpage_content}>
+          <div className={styles.productDetail}>
+            <Row>
+              <Col xs={6}>Left side</Col>
+              <Col>Right side</Col>
+            </Row>
+          </div>
+
+          <div className={styles.productDescription}>
+            <Row>
+              <Col>
+              Xüsusiyyətlər
+              </Col>
+            </Row>
+          </div>
+          <div className={styles.comments_side}>
+            <Row>
+              <Col>
+              İstifadəçi rəyləri
+              </Col>
+            </Row>
+          </div>
+          <div className={styles.similiarProducts}>
+            <Row>
+              <Col>
+              Oxşar məhsullar
+              </Col>
+            </Row>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ProductPage;

e02ac963cd418b133a782adb424ccf4a7349f859
lemanshamilova <lemansamilova2003@gmail.com>
2024-09-27 15:07:53 +0400

    hello

--- src/Pages/ProductPage/ProductPage.jsx
+++ src/Pages/ProductPage/ProductPage.jsx
@@ -1,6 +1,7 @@
 import styles from "./ProductPage.module.scss";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Reviews from "../../components/Reviews/Reviews";
 
 const ProductPage = () => {
   return (
@@ -24,7 +25,7 @@ const ProductPage = () => {
           <div className={styles.comments_side}>
             <Row>
               <Col>
-              İstifadəçi rəyləri
+              <Reviews/>
               </Col>
             </Row>
           </div>

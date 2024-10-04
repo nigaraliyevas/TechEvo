b7ee29bcc63a0ef8bc838ffb350ed8e85d7b3293
nigaraliyevas <naliyeva965@gmail.com>
2024-10-04 17:39:55 +0400

    added link for navigation

--- src/components/CardPC/CardPC.jsx
+++ src/components/CardPC/CardPC.jsx
@@ -4,6 +4,7 @@ import Basket from "/assets/images/HomePage/Products/basket.png";
 
 import styles from "./CardPC.module.scss";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 const CardPC = () => {
   const { filteredProducts } = useSelector(state => state.filter);
   const { currentPage, itemsPerPage } = useSelector(state => state.pagination);
@@ -16,42 +17,40 @@ const CardPC = () => {
       {currentItems.length === 0 ? (
         <div className={styles.noProductsMessage}>There are no products.</div>
       ) : (
-
         currentItems.map(card => (
-
           <div key={card.id} className={styles.card}>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <div>
-              <div style={{ position: "relative", overflow: "hidden" }}>
-                <img className={styles.cardImg} src={card.image} alt={card.name} />
-              </div>
-              <div className={styles.heartSpan}>
-                <PiHeartBold />
-              </div>
+            <Link to="/product" style={{textDecoration:"none"}}>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+              <div>
+                <div style={{ position: "relative", overflow: "hidden" }}>
+                  <img className={styles.cardImg} src={card.image} alt={card.name} />
+                </div>
+                <div className={styles.heartSpan}>
+                  <PiHeartBold />
+                </div>
 
-              <div className={styles.mailTitle}>
-                <div className={styles.namePrice}>
-                  <h4>{card.name}</h4>
-                  <p>{card.price} Azn</p>
-                </div>
-                <div className={styles.ratingBasket}>
-                  <div>
-                    <img src={card.rating} alt="rating" />
+                <div className={styles.mailTitle}>
+                  <div className={styles.namePrice}>
+                    <h4>{card.name}</h4>
+                    <p>{card.price} Azn</p>
                   </div>
-                  <div className={styles.basketBg}>
-                    <img src={Basket} />
+                  <div className={styles.ratingBasket}>
+                    <div>
+                      <img src={card.rating} alt="rating" />
+                    </div>
+                    <div className={styles.basketBg}>
+                      <img src={Basket} />
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
+            </Link>
           </div>
-
         ))
       )}
-
     </>
   );
 };

f2170aff4b1e8aba89e40aa90caec4561ea03dee
MahmudovSoltan <mahmudovsoltan477@gmail.com>
2024-10-23 20:32:17 +0400

    active class header

--- src/components/DetailImage/DetailImageComponent.jsx
+++ src/components/DetailImage/DetailImageComponent.jsx
@@ -1,30 +1,21 @@
-import image1 from "../../assets/images/DetailImage/unsplash_Hin-rzhOdWs.png";
-import image2 from "../../assets/images/DetailImage/unsplash_2.png";
-import image3 from "../../assets/images/DetailImage/unsplash_3.png";
-import image4 from "../../assets/images/DetailImage/unsplash_4.png";
-
 import { useState, useEffect } from "react";
 import style from "./DetailImage.module.scss";
 
-const DetailImageComponent = ({ setModalShow, product }) => {
+const DetailImageComponent = ({ setModalShow, product,setCarouselImages }) => {
   const [detailImage, setDetailImage] = useState([]);
   const [mainImage, setMainImage] = useState(null);
 
-  // UseEffect to set images once product is available
+
   useEffect(() => {
-    if (product?.imageUrl
-    ) {
-      setDetailImage(product.imageUrl
-      );
-      setMainImage(product.imageUrl
-        [0]); // Set the first image as main image
+    if (product?.imageUrl) {
+      setDetailImage(product.imageUrl);
+      setMainImage(product.imageUrl[0]); 
     }
   }, [product]);
 
-  console.log(mainImage,"mainImage");
-  
+  console.log(mainImage, "mainImage");
 
-  // Render loading or empty state if images are not ready
+
   if (!detailImage || detailImage.length === 0) {
     return <div>Loading images...</div>;
   }
@@ -37,13 +28,15 @@ const DetailImageComponent = ({ setModalShow, product }) => {
             className={style.detail_small_image}
             key={index}
             onClick={() => setMainImage(item)}
-            src={item} // Assuming item is the image URL
-            alt={`Product image ${index + 1}`}
+            src={item}
+            alt={`Product image ${index + 1}`} // Düzgün alt formatı
           />
         ))}
       </div>
       <img
-        onClick={() => setModalShow(true)}
+        onClick={() => {setModalShow(true)
+          setCarouselImages(product.imageUrl)
+        }}
         className={style.detail_big_image}
         src={mainImage} // Assuming mainImage is the image URL
         alt="Main Product Image"

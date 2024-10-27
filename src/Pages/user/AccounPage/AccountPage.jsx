4c0273e7512017bfb6896c8b1a77c1e24377648c
MahmudovSoltan <mahmudovsoltan477@gmail.com>
2024-10-27 14:48:51 +0400

    account page right

--- src/Pages/user/AccounPage/AccountPage.jsx
+++ src/Pages/user/AccounPage/AccountPage.jsx
@@ -4,6 +4,7 @@ import order from "../../../assets/images/account/accountIcon/orderIcon.svg"
 import userIcon from "../../../assets/images/account/accountIcon/userIcon.svg"
 import heart from "../../../assets/images/account/accountIcon/heartIcon.svg"
 import out from "../../../assets/images/account/accountIcon/logout.svg"
+import password from '../../../assets/images/account/accountIcon/password.svg'
 const AccountPage = () => {
   return (
     
@@ -36,6 +37,54 @@ const AccountPage = () => {
             <div className={style.account_info}>
             Hesab məlumatları
             </div>
+            <form action="">
+              <div className={style.user_inputs}>
+                <div className={style.user_input}>
+                    <div className={style.user_name}>
+                      <label htmlFor="">Ad</label>
+                      <div className={style.user_input}>
+                        <input type="text" placeholder="Ad" />
+                      </div>
+                    </div>
+                    <div className={style.user_name}>
+                      <label htmlFor="">Soyad</label>
+                      <div className={style.user_input}>
+                        <input type="text" placeholder="Soyad" />
+                      </div>
+                    </div>
+                </div>
+                <div className={style.user_input}>
+                    <div className={style.user_name}>
+                      <label htmlFor="">E-mail</label>
+                      <div className={style.user_input}>
+                        <input type="email" placeholder="E-mail" />
+                      </div>
+                    </div>
+                    <div className={style.user_name}>
+                      <label htmlFor="">Ünvan</label>
+                      <div className={style.user_input}>
+                        <input type="text" placeholder="Ünvan" />
+                      </div>
+                    </div>
+                </div>
+                <div className={style.user_input}>
+                    <div className={style.user_name}>
+                      <label htmlFor="">Şifrə</label>
+                      <div className={style.user_input}>
+                        <input type="password" placeholder="Şifrə" />
+                      </div>
+                      <div className={style.user_password}>
+                      Şifrəni sıfırla <img src={password} alt="" />
+                      </div>
+                    </div>
+                </div>
+
+              </div>
+              <div className={style.user_btn}>
+              <button  type="submit">Yadda saxla</button>
+
+              </div>
+            </form>
 
         </div>
       </div>

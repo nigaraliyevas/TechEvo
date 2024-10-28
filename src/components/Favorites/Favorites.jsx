15e8d0dff0937959a091e598a10e0df9187a0b80
Nasiba1719 <salmanli.91@mail.ru>
2024-10-29 01:47:20 +0400

    favorites loadinggg

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,34 +1,99 @@
+
 import React from "react";
 import { useSelector } from "react-redux";
-import { useAddFavoriteMutation, useGetFavoritesQuery } from "./../../redux/slices/favoritesSlice";
 import Card from "../../pages/user/HomePage/Section/Card/Card";
 
-const FavoritesPage = () => {
-  const accessToken = useSelector((state) => state.favorites.accessToken);
-  const { data: favorites, error, isLoading } = useGetFavoritesQuery();
-  const [addFavorite] = useAddFavoriteMutation();
-
-  const handleAddToFavorite = (productId) => {
-    if (!accessToken) {
-      console.error("Access token tapılmadı!");
-      return;
-    }
-    addFavorite(productId);
-  };
-
-  if (isLoading) return <p>Yüklənir...</p>;
-  if (error) return <p>Səhv baş verdi: {error.message}</p>;
+function Favorites() {
+  const favorites = useSelector((state) => state.favorites);
 
   return (
     <div>
-      <h2>Sevimlilər</h2>
-      <ul>
-        {favorites?.map((item) => (
-          <Card key={item.id} card={item} onAddToFavorite={handleAddToFavorite} />
+      <h2>Sevimlər</h2>
+      <div>
+        {favorites.map((favoriteItem) => (
+          <Card key={favoriteItem.id} card={favoriteItem} />
         ))}
-      </ul>
+      </div>
     </div>
   );
-};
+}
 
-export default FavoritesPage;
+export default Favorites;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from "react";
+// import { useSelector } from "react-redux";
+// import { useAddFavoriteMutation, useGetFavoritesQuery } from "./../../redux/slices/favoritesSlice";
+// import Card from "../../pages/user/HomePage/Section/Card/Card";
+
+// const FavoritesPage = () => {
+//   const accessToken = useSelector((state) => state.favorites.accessToken);
+//   const { data: favorites, error, isLoading } = useGetFavoritesQuery();
+//   const [addFavorite] = useAddFavoriteMutation();
+
+//   const handleAddToFavorite = (productId) => {
+//     if (!accessToken) {
+//       console.error("Access token tapılmadı!");
+//       return;
+//     }
+//     addFavorite(productId);
+//   };
+
+//   if (isLoading) return <p>Yüklənir...</p>;
+//   if (error) return <p>Səhv baş verdi: {error.message}</p>;
+
+//   return (
+//     <div>
+//       <h2>Sevimlilər</h2>
+//       <ul>
+//         {favorites?.map((item) => (
+//           <Card key={item.id} card={item} onAddToFavorite={handleAddToFavorite} />
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default FavoritesPage;
+
+
+
+
+
+
+
+// // import React from 'react'
+// // import {products} from '../../../src/products.js'
+
+// // const Favorites = ({ data = products }) => {
+// //   console.log(data)
+// //   const {name, price} = data
+// //   return (
+// //     <div>Favorites
+// //       {data.map((item, index) => (
+// //         <div key={index}>
+// //           <p>{item.name}</p>
+// //           <p>{item.price}</p>
+// //         </div>
+// //       ))}
+// //     </div>
+// //   )
+// // }
+
+// // export default Favorites
--- src/layouts/user/UserLayout.jsx
+++ src/layouts/user/UserLayout.jsx
@@ -8,9 +8,9 @@ import { Outlet } from "react-router-dom"
 const UserLayout = () => {
     return (
         <>
-            {/* <Header /> */}
+            <Header />
             <Outlet />
-            {/* <Footer /> */}
+            <Footer />
         </>
     )
 }

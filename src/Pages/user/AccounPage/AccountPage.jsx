ce2da8111d86544a88cf802713349eca133fb84c
Nasiba1719 <salmanli.91@mail.ru>
2024-10-29 01:47:20 +0400

    favorites loadinggg

--- src/Pages/user/AccounPage/AccountPage.jsx
+++ src/Pages/user/AccounPage/AccountPage.jsx
@@ -5,35 +5,38 @@ import userIcon from "../../../assets/images/account/accountIcon/userIcon.svg"
 import heart from "../../../assets/images/account/accountIcon/heartIcon.svg"
 import out from "../../../assets/images/account/accountIcon/logout.svg"
 import password from '../../../assets/images/account/accountIcon/password.svg'
+import Favorites from "../../../components/Favorites/Favorites";
 const AccountPage = () => {
+
   return (
-    
+
     <div className={style.container}>
-        <div className={style.account}>
-      <div className={style.account_container}>
-        <div className={style.account_left}> 
-                <div className={style.user_image}>
-                    <img src={userprofile} alt="" />
-                </div>
-                <div className={style.user_info}>
-                        <div className={style.uer_info_item}>
-                            <img src={userIcon} alt="" />
-                            <div>Hesab</div>
-                        </div>
-                        <div className={style.uer_info_item}>
-                            <img src={order} alt="" />
-                            <div>Sifarişlər</div>
-                        </div>
-                        <div className={style.uer_info_item}>
-                            <img src={heart} alt="" />
-                            <div>Sevimlilər</div>
-                        </div>
-                </div>
-                <div className={style.user_out}>
-                   <img src={out} alt="" />  Çıxış
-                </div>
-        </div>
-        <div className={style.account_right}>
+      <div className={style.account}>
+        <div className={style.account_container}>
+          <div className={style.account_left}>
+            <div className={style.user_image}>
+              <img src={userprofile} alt="" />
+            </div>
+            <div className={style.user_info}>
+              <div className={style.uer_info_item}>
+                <img src={userIcon} alt="" />
+                <div>Hesab</div>
+              </div>
+              <div className={style.uer_info_item}>
+                <img src={order} alt="" />
+                <div>Sifarişlər</div>
+              </div>
+              <div className={style.uer_info_item}>
+                <img src={heart} alt="" />
+                <div>Sevimlilər</div>
+
+              </div>
+            </div>
+            <div className={style.user_out}>
+              <img src={out} alt="" />  Çıxış
+            </div>
+          </div>
+          {/* <div className={style.account_right}>
             <div className={style.account_info}>
             Hesab məlumatları
             </div>
@@ -86,10 +89,11 @@ const AccountPage = () => {
               </div>
             </form>
 
+        </div> */}
+          <Favorites />
         </div>
-      </div>
 
-        </div>
+      </div>
     </div>
   );
 };
--- src/pages/user/HomePage/Section/Card/Card.jsx
+++ src/pages/user/HomePage/Section/Card/Card.jsx
@@ -1,15 +1,34 @@
 import React, { useState } from "react";
 import { PiHeartBold } from "react-icons/pi";
+import { TiHeartFullOutline } from "react-icons/ti";
 import { SlBasket } from "react-icons/sl";
 import StarRating from "../../../../../components/Rating/StarRating";
 import style from "../../HomePage.module.scss";
 import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { addToFavorites, removeFromFavorites } from "../../../../../redux/slices/favoritesSlice";
 
-function Card({ card, onAddToFavorite }) {
+function Card({ card }) {
   const { name, price, imageUrl, rating, id } = card;
+  const dispatch = useDispatch();
+  const favorites = useSelector((state) => state.favorites);
+
+  // Favoritdə olub-olmadığını yoxlamaq üçün state əlavə edin
+  const isFavorite = favorites.some((fav) => fav.id === id);
 
   const [selectedImage, setSelectedImage] = useState(0);
   const [lastMouseX, setLastMouseX] = useState(null);
+
+  const handleToggleFavorite = (event) => {
+    event.stopPropagation();
+    event.preventDefault();
+
+    if (isFavorite) {
+      dispatch(removeFromFavorites(id));
+    } else {
+      dispatch(addToFavorites(card));
+    }
+  };
 
   const handleMouseMove = (e) => {
     const { clientX } = e;
@@ -32,77 +51,82 @@ function Card({ card, onAddToFavorite }) {
     }
   };
 
+
   const handleDivClick = (index) => {
     setSelectedImage(index);
   };
 
   return (
-    <div className={style.cardContainer}>
-      <Link style={{ textDecoration: "none" }} to={`/product?id=${id}`} className={style.card}>
-        <span className={style.cardAnimationSpan}></span>
-        <span className={style.cardAnimationSpan}></span>
-        <span className={style.cardAnimationSpan}></span>
-        <span className={style.cardAnimationSpan}></span>
+    <Link style={({ textDecoration: "none" })} to={`/product?id=${id}`} className={style.card}>
+      <span className={style.cardAnimationSpan}></span>
+      <span className={style.cardAnimationSpan}></span>
+      <span className={style.cardAnimationSpan}></span>
+      <span className={style.cardAnimationSpan}></span>
 
-        <div style={{ position: "relative" }}>
+      <div style={{ position: "relative" }}>
+        <div
+          className={style.cardImgContainer}
+          onMouseMove={handleMouseMove} // Mouse üçün
+          // Mobil üçün toxunma hadisəsi
+          style={{ overflow: "hidden" }}
+        >
           <div
-            className={style.cardImgContainer}
-            onMouseMove={handleMouseMove}
-            style={{ overflow: "hidden" }}
+            className={style.imageSlider}
+            style={{
+              transform: `translateX(-${selectedImage * 25}%)`,
+              width: `${imageUrl.length * 100}%`,
+              height: "100%",
+            }}
           >
-            <div
-              className={style.imageSlider}
-              style={{
-                transform: `translateX(-${selectedImage * 25}%)`,
-                width: `${imageUrl.length * 100}%`,
-                height: "100%",
-              }}
-            >
-              {imageUrl.map((imgSrc, index) => (
-                <img
-                  key={index}
-                  className={style.cardImg}
-                  src={imgSrc}
-                  alt={name}
-                  style={{ width: `${100 / imageUrl.length}%` }}
-                />
-              ))}
-            </div>
-          </div>
-
-          <div className={style.radioButtons}>
-            {imageUrl.map((_, index) => (
-              <div
+            {imageUrl.map((imgSrc, index) => (
+              <img
                 key={index}
-                className={`${style.radioDiv} ${selectedImage === index ? style.selected : ""
-                  }`}
-                onClick={() => handleDivClick(index)}
+                className={style.cardImg}
+                src={imgSrc}
+                alt={name}
+                style={{ width: `${100 / imageUrl.length}%` }}
               />
             ))}
           </div>
+        </div>
 
-          <div className={style.heartSpan} onClick={() => onAddToFavorite(id)}>
-            <PiHeartBold />
+        <div className={style.radioButtons}>
+          {imageUrl.map((_, index) => (
+            <div
+              key={index}
+              className={`${style.radioDiv} ${selectedImage === index ? style.selected : ""
+                }`}
+              onClick={() => handleDivClick(index)} // Mouse üçün klik
+            // Mobil üçün toxunma
+            />
+          ))}
+        </div>
+
+        <div className={style.heartSpan} onClick={handleToggleFavorite}>
+          {isFavorite ? (
+            <TiHeartFullOutline style={{ color: "white" }} />
+          ) : (
+            <PiHeartBold style={{ fill: "white" }} />
+          )}
+        </div>
+
+        <div className={style.cardBottomTitles}>
+          <div className={style.namePrice}>
+            <h4>{name}</h4>
+            <p>{price} AZN</p>
           </div>
 
-          <div className={style.cardBottomTitles}>
-            <div className={style.namePrice}>
-              <h4>{name}</h4>
-              <p>{price} AZN</p>
-            </div>
-
-            <div className={style.ratingBasket}>
-              <StarRating value={rating} />{" "}
-              <div className={style.basketBg}>
-                <a href="#">
-                  <SlBasket style={{ width: "18px", height: "18px" }} />
-                </a>
-              </div>
+          <div className={style.ratingBasket}>
+            <StarRating value={rating} />{" "}
+            <div className={style.basketBg}>
+              <a href="#">
+                <SlBasket style={{ width: "18px", height: "18px" }} />
+              </a>
             </div>
           </div>
         </div>
-      </Link>
-    </div>
+      </div>
+    </Link>
   );
 }
 

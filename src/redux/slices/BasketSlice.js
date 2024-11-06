b05c883f50148af354a92baa935663f9d295d3e7
MahmudovSoltan <mahmudovsoltan477@gmail.com>
2024-11-06 22:41:47 +0400

    basket count

--- src/redux/slices/BasketSlice.js
+++ src/redux/slices/BasketSlice.js
@@ -9,16 +9,31 @@ const fetchLocalStorage = () => {
   }
 };
 
+const fetchLocalCount = () => {
+  let count = localStorage.getItem("count");
+  if (count) {
+    return JSON.parse(count);
+  } else {
+    return 0; 
+  }
+};
+
 const storeLocalStorage = (data) => {
   return localStorage.setItem("basket", JSON.stringify(data));
+};
+
+const storeLocalCount = (count) => {
+  return localStorage.setItem("count", JSON.stringify(count));
 };
 
 const calculateTotalPrice = (basket) => {
   return basket.reduce((total, item) => total + item.price * item.count, 0);
 };
+
 const initialState = {
   basket: fetchLocalStorage(),
   totalPrice: calculateTotalPrice(fetchLocalStorage()),
+  count: fetchLocalCount()<0 ? 0: fetchLocalCount(), 
 };
 
 const basketSlice = createSlice({
@@ -34,44 +49,51 @@ const basketSlice = createSlice({
       } else {
         state.basket.push({ ...action.payload, count: 1 });
       }
+      state.count++;  
       state.totalPrice = calculateTotalPrice(state.basket);
       storeLocalStorage(state.basket);
+      storeLocalCount(state.count);
     },
     removeCart: (state, action) => {
-      state.basket = state.basket.filter((item) => item.id !== action.payload);
-      storeLocalStorage(state.basket);
-      state.totalPrice = calculateTotalPrice(state.basket);
-    },
-    decrimentCount: (state, action) => {
-      const existsItem = state.basket.find(
-        (item) => item.id === action.payload
-      );
+      const existsItem = state.basket.find((item) => item.id === action.payload);
 
       if (existsItem) {
-        existsItem.count++;
-        state.totalPrice = calculateTotalPrice(state.basket);
-        storeLocalStorage(state.basket);
+        state.count -= existsItem.count;
+        state.basket = state.basket.filter((item) => item.id !== action.payload);
       }
+      state.totalPrice = calculateTotalPrice(state.basket);
+      storeLocalStorage(state.basket);
+      storeLocalCount(state.count);
     },
     incrementCount: (state, action) => {
       const existsItem = state.basket.find(
         (item) => item.id === action.payload
       );
-      if (existsItem) {
-        if (existsItem.count > 1) {
-          existsItem.count--;
-        } else if(existsItem.count > 1) {
-          state.basket = state.basket.filter(
-            (item) => item.id !== action.payload
-          );
+      if (existsItem && existsItem.count > 1) {
+        existsItem.count--;
+        state.count--; 
+        if (existsItem.count === 0) {
+          state.basket = state.basket.filter((item) => item.id !== action.payload);
         }
+      }
+      state.totalPrice = calculateTotalPrice(state.basket);
+      storeLocalStorage(state.basket);
+      storeLocalCount(state.count);
+    },
+    decrimentCount: (state, action) => {
+      const existsItem = state.basket.find(
+        (item) => item.id === action.payload
+      );
+      if (existsItem) {
+        existsItem.count++;
+        state.count++;
         state.totalPrice = calculateTotalPrice(state.basket);
         storeLocalStorage(state.basket);
+        storeLocalCount(state.count);
       }
     },
   },
 });
 
-export const { addToCart, removeCart, incrementCount, decrimentCount } =
-  basketSlice.actions;
+export const { addToCart, removeCart, incrementCount, decrimentCount } = basketSlice.actions;
 export default basketSlice.reducer;

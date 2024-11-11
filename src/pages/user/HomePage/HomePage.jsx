e9153779f1ef87d696b190c63a26ccaac58c063f
Gulay <gulaymovlamova@gmail.com>
2024-11-11 15:03:09 +0400

    m

--- src/pages/user/HomePage/HomePage.jsx
+++ src/pages/user/HomePage/HomePage.jsx
@@ -1,5 +1,5 @@
-import styles from "./HomePage";
-import InternCr from "../../../assets/images/products/InternCr";
+import styles from "./HomePage.module.scss";
+import InternCr from "../../../assets/images/products/InternCr.png";
 import Delivery from "../../../assets/images/products/Delivery.png";
 import Repair from "../../../assets/images/products/Repair.png";
 import SecondHand from "../../../assets/images/products/SecondHand.png";
@@ -7,74 +7,60 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Section from "./Section/Section";
 import SliderComponent from "./Section/Slider/Slider";
-import { useGetProductsByCategoryNameQuery } from "../../../redux/sercives/productApi";
+
 
 const HomePage = () => {
-  const { data: laptopsData, isLoading: isLaptopsLoading } = useGetProductsByCategoryNameQuery("Laptop");
-  const { data: mousesData, isLoading: isMousesLoading } = useGetProductsByCategoryNameQuery("Mouse");
-  const { data: klaviaturasData, isLoading: isKlaviaturasLoading } = useGetProductsByCategoryNameQuery("Klaviatura");
-
-    // useGetProductsByCategoryNameQuery("");
-
-
   return (
     <>
       <section>
-        <div>
-          <SliderComponent />
+        <div className="" style={{ paddingTop: "93px" }}>
+          <SliderComponent/>
         </div>
-
-
       </section>
+
       <section id={styles.specialselected}>
         <div className={styles.container_bottom}>
           <h3 className={styles.specialH3}>Xüsusi Seçimlər</h3>
-          <Section title="PC" data={laptopsData} />
-          <Section title="Mouse" data={mousesData} />
-          <Section title="Keyboard" data={klaviaturasData}/> 
-          <div className={styles.servicesDiv}>
-            <div className={styles.services}>Xidmətlərimiz</div>
-            <div className={styles.mainServices}>
-              <div className={` ${styles.border} ${styles.iconsCenter}`}>
-                <img src={InternCr} />
-                <div className={styles.text}>
-                  <h3>Daxili kredit</h3>
-                  <p>
-                    Bizim kredit təkliflərimizlə Tech məhsulları sizin büdcənizə
-                    uyğun olacaq.
-                  </p>
-                </div>
-              </div>
-              <div className={`${styles.border} ${styles.iconsCenter}`}>
-                <img src={Delivery} />
+          <Section title="Pc"/>
 
-                <div className={styles.text}>
-                  <h3>Çatdırılma</h3>
-                  <p>
-                    Bizim kredit təkliflərimizlə Tech məhsulları sizin büdcənizə
-                    uyğun olacaq.
-                  </p>
-                </div>
+          <Section title="Mouse"/>
+
+          <Section title="Keyboard"/>
+
+
+          <div className={styles.services}>Xidmətlərimiz</div>
+
+          <div className={styles.mainServices}>
+            <div className={` ${styles.border} ${styles.iconsCenter}`}>
+              <img src={InternCr} />
+              <div className={styles.text}>
+                <h3>Daxili kredit</h3>
+                <p>Bizim kredit təkliflərimizlə Tech məhsulları sizin büdcənizə uyğun olacaq.</p>
               </div>
-              <div className={`${styles.border} ${styles.iconsCenter}`}>
-                <img src={Repair} />
-                <div className={styles.text}>
-                  <h3>Təmir</h3>
-                  <p>
-                    Bizim kredit təkliflərimizlə Tech məhsulları sizin büdcənizə
-                    uyğun olacaq.
-                  </p>
-                </div>
+            </div>
+
+            <div className={`${styles.border} ${styles.iconsCenter}`}>
+              <img src={Delivery} />
+
+              <div className={styles.text}>
+                <h3>Çatdırılma</h3>
+                <p>Bizim kredit təkliflərimizlə Tech məhsulları sizin büdcənizə uyğun olacaq.</p>
               </div>
-              <div className={`${styles.border} ${styles.iconsCenter}`}>
-                <img src={SecondHand} />
-                <div className={styles.text}>
-                  <h3>İkinci əl satış</h3>
-                  <p>
-                    Bizim kredit təkliflərimizlə Tech məhsulları sizin büdcənizə
-                    uyğun olacaq.
-                  </p>
-                </div>
+            </div>
+
+            <div className={`${styles.border} ${styles.iconsCenter}`}>
+              <img src={Repair} />
+              <div className={styles.text}>
+                <h3>Təmir</h3>
+                <p>Bizim kredit təkliflərimizlə Tech məhsulları sizin büdcənizə uyğun olacaq.</p>
+              </div>
+            </div>
+
+            <div className={`${styles.border} ${styles.iconsCenter}`}>
+              <img src={SecondHand} />
+              <div className={styles.text}>
+                <h3>İkinci əl satış</h3>
+                <p>Bizim kredit təkliflərimizlə Tech məhsulları sizin büdcənizə uyğun olacaq.</p>
               </div>
             </div>
           </div>
--- src/pages/user/HomePage/Section/Section.jsx
+++ src/pages/user/HomePage/Section/Section.jsx
@@ -1,93 +1,60 @@
-
-import { useEffect, useRef, useState } from "react";
+import React, { useRef } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Navigation } from "swiper/modules";
 // Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/autoplay';
+import 'swiper/css/controller';
+import 'swiper/css/navigation';
+import Card from './Card/Card';
+import { products } from '../../../../products';
+import styles from '../HomePage.module.scss';
 
-import "swiper/css";
-import "swiper/css/autoplay";
-import "swiper/css/controller";
-import "swiper/css/navigation";
-import Card from "./Card/Card";
-import { products } from "../../../../products";
-import styles from "../HomePage.module.scss";
+const Section = ({ title }) => {
+    // Swiper instance-i referens vasitəsilə saxlayırıq
+    const swiperRef = useRef(null);
 
+    return (
+        <div>
+            <div className={styles.title}>{title}</div>
 
-
-const Section = ({ title, data = products }) => {
-  const swiperRef = useRef(null);
-
-  // Ekran ölçüsünü saxlamaq üçün state
-  const [isMobile, setIsMobile] = useState(window.innerWidth <= 480);
-
-  // Ekran ölçüsünü izləmək üçün useEffect
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth <= 480);
-    };
-
-    window.addEventListener('resize', handleResize);
-
-    // Cleanup
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-
-  return (
-    <div>
-      <div className={styles.title}>{title}</div>
-
-      <div className={styles.cardMain}>
-        {isMobile ? (
-          // Mobil cihazlarda kartları 1.5 card göstərəcək sadə düzən
-          <div className={styles.mobileCards}>
-            {data.map((card) => (
-              <div className={styles.cardContainer} key={card.id}>
-                <Card card={card} />
-              </div>
-            ))}
-          </div>
-        ) : (
-          // Desktop üçün Swiper
-          <Swiper
-            effect="fade"
-            className={styles.swiper}
-            spaceBetween={30}
-            slidesPerView={3}
-            autoplay={{
-              delay: 400000,
-              disableOnInteraction: false,
-            }}
-            loop={true}
-            navigation={{
-              nextEl: ".swiper-button-next",
-              prevEl: ".swiper-button-prev",
-            }}
-            modules={[Autoplay, Navigation]}
-            onSwiper={(swiper) => {
-              swiperRef.current = swiper;
-            }}
-          >
-            {data.map((card) => (
-              <SwiperSlide className={styles.swiperSlide} key={card.id}>
-                <div
-                  className={styles.cardContainer}
-                  onMouseEnter={() => swiperRef.current.autoplay.stop()}
-                  onMouseLeave={() => swiperRef.current.autoplay.start()}
+            <div className={styles.cardMain}>
+                <Swiper
+                    effect="fade"
+                    className={styles.swiper}
+                    spaceBetween={30}
+                    slidesPerView={3}
+                    autoplay={{
+                        delay: 5000,
+                        disableOnInteraction: false,
+                    }}
+                    loop={true}
+                    navigation={{
+                        nextEl: ".swiper-button-next",
+                        prevEl: ".swiper-button-prev", // Sol ox
+                    }}
+                    modules={[Autoplay, Navigation]}
+                    onSwiper={(swiper) => {
+                        swiperRef.current = swiper; // Swiper instance-in referensə əlavə edirik
+                    }}
                 >
-                  <Card card={card} />
-                </div>
-              </SwiperSlide>
-            ))}
-            {/* Kənar oxlar */}
-            <div className={`${styles.swiperNav} ${styles.swiperNavPrev} swiper-button-prev`}></div>
-            <div className={`${styles.swiperNav} ${styles.swiperNavNext} swiper-button-next`}></div>
-          </Swiper>
-        )}
-      </div>
-    </div>
-  );
+                    {products.map((card) => (
+                        <SwiperSlide className={styles.swiperSlide} key={card.id}>
+                            <div
+                                className={styles.cardContainer}
+                                onMouseEnter={() => swiperRef.current.autoplay.stop()} // Hover zamanı autoplay dayandırılır
+                                onMouseLeave={() => swiperRef.current.autoplay.start()} // Hover-dən çıxanda autoplay başlatılır
+                            >
+                                <Card card={card} />
+                            </div>
+                        </SwiperSlide>
+                    ))}
+                    <div className={`${styles.swiperNav} ${styles.swiperNavPrev} swiper-button-prev`}></div> {/* Sol ox */}
+                    <div className={`${styles.swiperNav} ${styles.swiperNavNext} swiper-button-next`}></div>
+                </Swiper>
+            </div>
+        </div>
+    );
 };
 
 export default Section;

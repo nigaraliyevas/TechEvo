8b949c3bda9956ab593885292673a4d7da0dc904
nigaraliyevas <naliyeva965@gmail.com>
2024-11-18 15:10:57 +0400

    returned Login

--- src/pages/user/Login/LoginPage.jsx
+++ src/pages/user/Login/LoginPage.jsx
@@ -7,7 +7,7 @@ import "../../../assets/common/base.scss";
 import { Link } from "react-router-dom";
 // import { login } from "../../../redux/slices/AuthSlice";
 import Button from "../../../components/Button/Button";
-// import useLogin from "../../../hooks/useLogin";
+import useLogin from "../../../hooks/useLogin";
 
 const LoginPage = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -18,7 +18,7 @@ const LoginPage = () => {
     setShowPassword(prev => !prev);
   };
 
-  // const login = useLogin();
+  const login = useLogin();
 
   const handleLogin = e => {
     e.preventDefault();

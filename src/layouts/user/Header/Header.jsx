2e8b4e238617b3094ca9b38e664d0f16226e6826
Soltan <mahmudovsoltan477@gmail.com>
2024-11-25 16:19:11 +0400

    add header favorites

--- src/layouts/user/Header/Header.jsx
+++ src/layouts/user/Header/Header.jsx
@@ -524,9 +524,54 @@ const Header = ({ exist, confirm }) => {
                     Hesabım
                   </NavLink>
                   <div className={styles.navbar_users_item}>
-                    <div>
-                      <div>Sevimlilər</div>
-                    </div>
+                    <NavLink to={"/favorites"}  className={styles.navbar_users_item}>
+                    <span>
+                      <svg
+                      className="favorites"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M12.62 20.8101C12.28 20.9301 11.72 20.9301 11.38 20.8101C8.48 19.8201 2 15.6901 2 8.6901C2 5.6001 4.49 3.1001 7.56 3.1001C9.38 3.1001 10.99 3.9801 12 5.3401C13.01 3.9801 14.63 3.1001 16.44 3.1001C19.51 3.1001 22 5.6001 22 8.6901C22 15.6901 15.52 19.8201 12.62 20.8101Z"
+                          stroke="#CCCCCC"
+                          stroke-width="1.5"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                        />
+                      </svg>
+                      <svg
+                        className="favorites_color"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <defs>
+                          <linearGradient
+                            id="grad"
+                            gradientTransform="rotate(102.09)"
+                          >
+                            <stop offset="4.1%" stop-color="#fd393f" />
+                            <stop offset="55.43%" stop-color="#db35e2" />
+                            <stop offset="94.96%" stop-color="#4e3dee" />
+                          </linearGradient>
+                        </defs>
+                        <path
+                          d="M12.62 20.8101C12.28 20.9301 11.72 20.9301 11.38 20.8101C8.48 19.8201 2 15.6901 2 8.6901C2 5.6001 4.49 3.1001 7.56 3.1001C9.38 3.1001 10.99 3.9801 12 5.3401C13.01 3.9801 14.63 3.1001 16.44 3.1001C19.51 3.1001 22 5.6001 22 8.6901C22 15.6901 15.52 19.8201 12.62 20.8101Z"
+                          stroke="url(#grad)"
+                          stroke-width="1.5"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                        />
+                      </svg>
+
+                    </span>
+                      Sevimlilər
+                    </NavLink>
                   </div>
                 </div>
               </div>

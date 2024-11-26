dfe6923dc78a7f04ad932bb4c4bfe9606ec26af4
nigaraliyevas <naliyeva965@gmail.com>
2024-11-26 14:07:34 +0400

    fiexed errors

--- src/router/UserRouter.jsx
+++ src/router/UserRouter.jsx
@@ -19,15 +19,11 @@ import AllOrders from "../components/Orders/AllOrders";
 import { useEffect } from "react";
 import { setTokens } from "../redux/slices/TokenSlice";
 import { useDispatch } from "react-redux";
-<<<<<<< HEAD
-import IdealPcPage from "../pages/user/IdealPcPage/IdealPcPage";
+import IdealPcPage from "../pages/user/IdealPcPage/IdealPcPageTemp";
 import Repair from "../pages/user/RepairPage/Repair";
 import Delivery from "../pages/user/DeliveryPage/Delivery";
-import Credit from "../pages/user/CreditPage/Credit";
-=======
-import IdealPcPage from "../Pages/user/IdealPcPage/IdealPcPage";
 import Favorites from "../components/Favorites/Favorites";
->>>>>>> 2e8b4e238617b3094ca9b38e664d0f16226e6826
+import Credit from "../pages/user/CreditPage/Credit";
 
 const UserRouter = ({ setConfirm, confirm, setExist, exist }) => {
   const dispatch = useDispatch();
@@ -75,7 +71,7 @@ const UserRouter = ({ setConfirm, confirm, setExist, exist }) => {
         <Route path="/repair" element={<Repair />} />
         <Route path="/delivery" element={<Delivery />} />
         <Route path="/credit" element={<Credit />} />
-        
+
         <Route path="/favorites" element={<Favorites />} />
       </Route>
     </Routes>

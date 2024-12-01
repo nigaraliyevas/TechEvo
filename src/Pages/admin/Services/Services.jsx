738d773df4f08d20145cefa27e4504857f7c6628
Gulay <gulaymovlamova@gmail.com>
2024-12-01 16:21:04 +0400

    fixed: admin routing system

---
+++ src/Pages/admin/Services/Services.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Services = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, vero. Ipsa
+      sed autem veniam ab officiis voluptatum inventore iusto perferendis modi
+      dicta animi voluptates dolore quas facilis sint, alias vitae iste
+      voluptate optio debitis ipsum deserunt expedita ad nesciunt. Ab maxime
+      tenetur sit tempora ipsum dolore reprehenderit, eveniet veritatis error
+      dicta rem delectus! Sit, nulla voluptate vitae quia, odio itaque
+      consectetur eum cum minima voluptas totam, sunt eveniet sed ex cumque!
+      Repellendus, eaque? Nostrum, quibusdam repellat optio eveniet quis
+      reiciendis voluptates laboriosam natus aliquam perspiciatis quaerat
+      ratione fugiat dolorum unde voluptatibus hic voluptas. Atque libero ad
+      consequatur minima reiciendis necessitatibus eius vitae natus sed labore
+      recusandae itaque vero laboriosam corrupti numquam, facere quos officiis,
+      quisquam, perspiciatis temporibus totam illum! Quidem quibusdam totam
+      suscipit nihil error, rem quae et molestiae explicabo dicta ratione
+      debitis nemo ut beatae placeat quasi reiciendis maiores repellat
+      laudantium voluptatem est provident aliquam quos? Neque, explicabo
+      repudiandae!
+    </div>
+  );
+};
+
+export default Services;

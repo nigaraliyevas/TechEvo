b22c9d586a0033dc979f0dc8698b9de94c4e7b61
Gulay <gulaymovlamova@gmail.com>
2024-12-01 16:21:04 +0400

    fixed: admin routing system

--- src/router/AdminRouter.jsx
+++ src/router/AdminRouter.jsx
@@ -1,21 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import AdminLayout from "../layouts/admin/AdminLayout";
 import Dashboard from "../pages/admin/DashboardPage";
+import Services from "../Pages/admin/Services/Services";
 
 const AdminRouter = () => {
   return (
     <Routes>
-      <Route
-        path="/"
-        element={
-          <AdminLayout>
-            <Routes>
-              <Route path="/" element={<Dashboard />} />
-              {/* <Route path="/another" element={<Dashboard />} /> */}
-            </Routes>
-          </AdminLayout>
-        }
-      />
+      <Route path="/" element={<AdminLayout />}>
+        <Route index element={<Dashboard />} />
+        <Route path="adminServices" element={<Services />} />
+      </Route>
     </Routes>
   );
 };
